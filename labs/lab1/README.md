b95db8bb45e291e7cbd78cd7393bfcd9cf9fb564
Lukas Elmlund <lukas.elmlund@gmail.com>
2023-01-20 16:28:36 +0100

    lab1: some more assignments

--- labs/lab1/README.md
+++ labs/lab1/README.md
@@ -1,8 +1,16 @@
-# Reflection question 1
+#Reflection question 1
 If a property is not found within an object it is evaluated to `undefined` 
 when trying to be read. Because of falsy characteristics `undefined === false`. 
 
-# Reflection question 2
+#Reflection question 2
 `for..in` iteration of js object iterates over enumerable properties both inherited and own.
-`Object.keys()` fetches only own enumerable properties. The function `sort()` is a prototype
-function from `Array`. 
+`Object.keys()` fetches only own enumerable properties. The result will be different if there
+are any inherited enumerable properties.
+The function `sort()` is a prototype function from `Array`, it is not enumerable or is inherited 
+__ask at lab!!__. 
+
+
+#Reflection question 3
+Classes are represented as functions. The functions have a property
+called prototype which is an object including 
+
--- labs/lab1/lab1.js
+++ labs/lab1/lab1.js
@@ -29,31 +29,38 @@ Person.prototype.birthday = function() { this.age++; };
 let per = new Person();
 
 console.log('\n\n Person')
+console.log("\nfor..in");
 for(let n in per){
     console.log(n +': ' + per[n] + ', is enum: ' + Object.propertyIsEnumerable(per[n]))
 }
 
+console.log("\no.keys");
+Object.keys(per).forEach(name => console.log(name));
 
-console.log('\n\nPerson2')
+
+console.log('\n\nPerson2');
 
 class Person2 {
-  static #count = 0;
   constructor(age) {
     this.age = age || 0;
-    Person2.#count = Person2.#count + 1;
   }
 
   birthday() {
     this.age++;
   }
- 
 }
 
 let per2 = new Person2();
 
+console.log("\nfor..in");
 for(let n in per2){
     console.log(n +': ' + per2[n] + ', is enum: ' + Object.propertyIsEnumerable(per2[n]))
 }
+
+
+console.log("\no.keys()");
+
+Object.keys(per2).forEach(name => console.log(name));
 /**
  * Reflection question 2
  */
@@ -61,18 +68,31 @@ for(let n in per2){
 console.log('\n--- Assignment 1 ---------------------------------------')
 
 function makeOptions(inv, prop) {
-  return 'TODO';
+    let keys = Object.keys(inv);
+    let filtered_keys = keys.filter(item => inv[item][prop]);
+    let price_pair = filtered_keys.map((key) => {return {name: key, price:inv[key].price}});
+    let html_string = price_pair.reduce((previous, current) => {
+        return previous + `<option value="${current.name}"> ${current.name}, ${current.price} kr</option>\n`
+    }, "");
+    return html_string; 
 }
 
 console.log(makeOptions(imported.inventory, 'foundation'));
 
 console.log('\n--- Assignment 2 ---------------------------------------')
 class Salad {
-  constructor() {}
-  add(name, properties) { }
-  remove(name) {}
+  constructor(salad) {
+        if (salad instanceof Salad) {
+            Object.assign(this, {...salad});
+        } else if (typeof salad === 'String') {
+            this.what = JSON.parse(salad);
+        } else {
+            this.ingridients = {};
+        }
+  }
+  add(name, properties) {this.ingridients[name] = properties;return this;}
+  remove(name) {delete this.ingridients[name];return this;}
 }
-/*
 let myCaesarSalad = new Salad()
   .add('Sallad', imported.inventory['Sallad'])
   .add('Kycklingfilé', imported.inventory['Kycklingfilé'])
@@ -84,16 +104,23 @@ let myCaesarSalad = new Salad()
 console.log(JSON.stringify(myCaesarSalad) + '\n');
 myCaesarSalad.remove('Gurka');
 console.log(JSON.stringify(myCaesarSalad) + '\n');
-*/
 console.log('\n--- Assignment 3 ---------------------------------------')
-//console.log('En ceasarsallad kostar ' + myCaesarSalad.getPrice() + 'kr');
+
+Salad.prototype.getPrice = function () {
+    return Object.values(this.ingridients).reduce((previous, current) => previous + current.price, 0);
+}
+
+Salad.prototype.count = function (prop) {
+    return Object.values(this.ingridients).filter((item) => item[prop]).length
+}
+console.log('En ceasarsallad kostar ' + myCaesarSalad.getPrice() + 'kr');
 // En ceasarsallad kostar 45kr
-//console.log('En ceasarsallad har ' + myCaesarSalad.count('lactose') + ' ingredienser med laktos');
+console.log('En ceasarsallad har ' + myCaesarSalad.count('lactose') + ' ingredienser med laktos');
 // En ceasarsallad har 2 ingredienser med laktos
-//console.log('En ceasarsallad har ' + myCaesarSalad.count('extra') + ' tillbehör');
+console.log('En ceasarsallad har ' + myCaesarSalad.count('extra') + ' tillbehör');
 // En ceasarsallad har 3 tillbehör
 
-/*
+
 console.log('\n--- reflection question 3 ---------------------------------------')
 console.log('typeof Salad: ' + typeof Salad);
 console.log('typeof Salad.prototype: ' + typeof Salad.prototype);
@@ -101,10 +128,13 @@ console.log('typeof Salad.prototype.prototype: ' + typeof Salad.prototype.protot
 console.log('typeof myCaesarSalad: ' + typeof myCaesarSalad);
 console.log('typeof myCaesarSalad.prototype: ' + typeof myCaesarSalad.prototype);
 console.log('check 1: ' + (Salad.prototype === Object.getPrototypeOf(myCaesarSalad)));
+console.log('Object.getPrototypeOf(myCesarSalad): ' + (Object.getPrototypeOf(myCaesarSalad)));
 console.log('check 2: ' + (Object.prototype === Object.getPrototypeOf(Salad.prototype)));
-*/
+console.log('Object.getPrototypeOf(Salad.prototype): ' + (Object.getPrototypeOf(Salad.prototype)));
+
+
 console.log('\n--- Assignment 4 ---------------------------------------')
-/*
+
 const objectCopy = new Salad(myCaesarSalad);
 const json = JSON.stringify(myCaesarSalad);
 const jsonCopy = new Salad(json);
@@ -114,7 +144,7 @@ console.log('copy from json\n' + JSON.stringify(jsonCopy));
 objectCopy.add('Gurka', imported.inventory['Gurka']);
 console.log('originalet kostar kostar ' + myCaesarSalad.getPrice() + ' kr');
 console.log('med gurka kostar den ' + objectCopy.getPrice() + ' kr');
-*/
+
 console.log('\n--- Assignment 5 ---------------------------------------')
 /*
 let myGourmetSalad = new GourmetSalad()
